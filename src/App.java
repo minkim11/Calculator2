ba00c3c65be5ff2f7702aeff9ff334453743db69
minkim11 <alsgur4772@gmail.com>
2026-03-19 17:46:43 +0900

    STEP2 완료 feat : Calculator 클래스 추가(결과값 리스트 생성, 계산 메서드, 게터, 세터, 삭제 메서드) fix : App 클래스 수정(계산 부분 Calculator 클래스로 분리)

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
@@ -6,20 +7,19 @@ public class App {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in); // 사용자 입력을 받기 위해 Scanner 객체 생성
+        Calculator calculator = new Calculator(); // 계산기 클래스 객체화
 
         System.out.println("===== 자바 계산기 =====");
-
-        // 입력 받을 변수 선언
-        int num1;
-        char op;
-        int num2;
         // 연산자 확인을 위한 리스트 생성
         List<Character> opList = List.of('+', '-', '*', '/');
 
         // 사용자 입력 후 계산 기능 반복
         while (true) {
-            double result = 0; // 결과값 초기화
-            // 사용자 입력 및 예외 처리
+            // 입력 받을 변수 선언
+            int num1;
+            char op;
+            int num2;
+            // 사용자 입력 및 숫자 이외의 값 입력 시 예외 처리
             try {
                 System.out.print("첫 번째 숫자를 입력하세요.: ");
                 num1 = sc.nextInt();
@@ -29,7 +29,7 @@ public class App {
                 num2 = sc.nextInt();
             } catch (InputMismatchException e) {
                 System.out.println("숫자를 입력하세요!");
-                sc.nextLine(); // 잘못된 입력값 제거
+                sc.nextLine(); // 잘못된 입력값 제거(다음 입력 받기 위해)
                 continue;
             }
             // 양의 정수 확인
@@ -37,37 +37,55 @@ public class App {
                 System.out.println("양의 정수를 입력하세요!");
                 continue;
             }
-
-            // 연산자 확인 및 계산 기능
-            if (opList.contains(op)) {
-                switch (op) {
-                    case '+':
-                        result = num1 + num2;
-                        break;
-                    case '-':
-                        result = num1 - num2;
-                        break;
-                    case '*':
-                        result = num1 * num2;
-                        break;
-                    case '/': // 나눗셈 오류 방지
-                        if (num2 == 0) {
-                            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                            continue;
-                        }
-                        result = num1 / (double)num2;
-                        break;
-                }
-            } else {
+            // 연산자 확인
+            if (!opList.contains(op)) {
                 System.out.println("올바른 연산자를 입력하세요! " + opList);
                 continue;
             }
+            // 나눗셈 오류 방지
+            if (op == '/' && num2 == 0) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                continue;
+            }
 
+            // 계산기 클래스의 메서드 사용
+            double result = calculator.calculate(num1, num2, op);
 
-            //결과값 출력
+            // 결과값 출력
             System.out.println("결과 = " + result);
 
-            //반복문 종료 분기점
+            // 결과 목록 조회(getter 활용)
+            ArrayList<Double> resultList = calculator.getResultList();
+            System.out.println("결과 목록 = " + resultList);
+
+            // 결과 목록 수정(setter 활용)
+            try {
+                System.out.print("결과 목록을 수정하시겠습니까? (yes 입력 시 진행): ");
+                String changeYes = sc.next();
+                if (changeYes.equals("yes")) {
+                    System.out.print("목록 중 변경할 값의 번호를 입력하세요. (0부터 시작): ");
+                    int index = sc.nextInt();
+                    System.out.print("원하는 변경값을 입력하세요: ");
+                    double changeResult = sc.nextDouble();
+                    calculator.setResultList(index, changeResult);
+                    System.out.println("변경 완료!");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("변경 실패! (번호와 값을 제대로 입력하세요.)");
+                sc.nextLine();
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("변경 실패! (목록에 없는 번호입니다.)");
+            }
+
+            // 결과값 삭제
+            System.out.print("첫번째 결과값을 삭제하시겠습니까? (yes 입력 시 진행): ");
+            String removeCheck = sc.next();
+            if (removeCheck.equals("yes")) {
+                calculator.removeResult();
+                System.out.println("삭제 완료!");
+            }
+
+            // 반복문 종료 분기점
             System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
             String exitCheck = sc.next();
             if (exitCheck.equals("exit")) {
